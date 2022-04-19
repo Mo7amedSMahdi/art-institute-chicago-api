5be02d0722f5ee34b8b5b8f1422b444040567224
Mo7amedSMahdi <Mo7ammed.salah@outlook.com>
2022-04-19 17:05:23 +0300

    display artworks counter in the header nav.
    - Add method to update the header nav with numbers of artworks.
    - Remove unnecessary files.

--- src/js/modules/nav.js
+++ src/js/modules/nav.js
@@ -1,16 +1,30 @@
 import { getArtWorksCount } from './counters.js';
-
 const nav = document.querySelector('.header-nav');
+
 const ArtWorksCount = async () => {
   return await getArtWorksCount().then((result) => result);
 };
 
+const renderNavCounters = async () => {
+  const artworksCount = await ArtWorksCount();
+  const links = nav.querySelectorAll('a');
+
+  links.forEach((link) => {
+    if (link.textContent === 'Artworks') {
+      const span = link.querySelector('span');
+      span.textContent = `(${artworksCount})`;
+    }
+  });
+};
+
 const renderNav = () => {
   const htmlElement = `<ul class="nav nav--main">
-                <li><a href="#">Artworks<span></a></li>
+                <li><a href="#">Artworks<span></span></a></li>
                 <li><a href="#">Artists</a></li>
             </ul>`;
   nav.innerHTML = htmlElement;
+
+  renderNavCounters();
 };
 
 export { renderNav };
