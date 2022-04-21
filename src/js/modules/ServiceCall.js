6e9db7e5f7b6441c14b275e2ccc38ba2743f8127
Mo7amedSMahdi <Mo7ammed.salah@outlook.com>
2022-04-21 21:02:09 +0300

    add API call to involvment API and implement add like

--- src/js/modules/ServiceCall.js
+++ src/js/modules/ServiceCall.js
@@ -1,4 +1,6 @@
 const baseUrl = 'https://api.artic.edu/api/v1/';
+const appID = 'Ea05s8iIppToZ9bgBQcY';
+const involvmentUrl = `https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/${appID}/`;
 
 const getArtWorks = async () => {
   const result = await fetch(`${baseUrl}artworks`, {
@@ -7,4 +9,18 @@ const getArtWorks = async () => {
   });
   return result.json();
 };
-export { getArtWorks };
+
+const addLike = async (artworkID) => {
+  const bodyData = {
+    item_id: artworkID,
+  };
+  await fetch(`${involvmentUrl}likes`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(bodyData),
+  });
+};
+
+export { getArtWorks, addLike };
--- src/js/modules/artworks.js
+++ src/js/modules/artworks.js
@@ -1,21 +1,23 @@
-import { getArtWorks } from './ServiceCall.js';
+import { getArtWorks, addLike } from './ServiceCall.js';
 
 const container = document.querySelector('.artworks-list');
-
+let likeButtons;
 const renderArtworks = async () => {
   let artworkElements = '';
   container.innerHTML = 'fetching data...';
   const artworkArray = await getArtWorks().then((result) => result);
+
   artworkArray.data.forEach((artwork) => {
     artworkElements += `<div data-id="${artwork.id}" class="artwork flex flex--column">
                       <div class="artwork-header">
                           <div class="header-image">
-                              <img src="https://www.artic.edu/iiif/2/${artwork.image_id}/full/843,/0/default.jpg" alt="${artwork.thumbnail.alt_text}">
+                              <img src="https://www.artic.edu/iiif/2/${artwork.image_id}/full/843,/0/default.jpg" alt="">
                           </div>
                       </div>
                       <div class="artwork-body flex">
                           <h2>${artwork.title}</h2>
-                          <img src="./images/heart.svg">
+                          <button data-target="like" data-id="${artwork.id}" type="button" class="btn btn--icon">
+                          <img src="./images/heart.svg" alt="heart icon"></button>
                       </div>
                       <div class="artwork-buttons flex flex--column">
                           <button type="button" class="btn btn--primary">Comments</button>
@@ -24,6 +26,15 @@ const renderArtworks = async () => {
                   </div>`;
   });
   container.innerHTML = artworkElements;
+  likeButtons = document.querySelectorAll('[data-target="like"]');
+  likeButtons.forEach((likeButton) => {
+    likeButton.addEventListener('click', async () => {
+      const artworkID = likeButton.dataset.id;
+      await addLike(artworkID);
+      const icon = likeButton.querySelector('img');
+      icon.src = './images/heart-filled.svg';
+    });
+  });
 };
 
 export { renderArtworks };
