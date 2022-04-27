10f5e5c1372226398d9a069c7f2608e2b99ffe27
Mo7amedSMahdi <Mo7ammed.salah@outlook.com>
2022-04-27 13:55:29 +0300

    finish the home page components

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import './App.css';
 import Header from './components/header/header';
 import Intro from './components/intro/Intro';
 import Exhibitions from './components/exhibitions/Exhibitions';
+import Artists from './components/artists/Artists';
+import Artworks from './components/artworks/Artworks';
+import Footer from './components/footer/Footer';
 
 function App() {
   return (
@@ -10,6 +13,9 @@ function App() {
       <main>
         <Intro />
         <Exhibitions />
+        <Artists />
+        <Artworks />
+        <Footer />
       </main>
     </>
   );
