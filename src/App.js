e6790f2b86f5cca307eb608c1a01e4dea4c26e9a
Mo7amedSMahdi <Mo7ammed.salah@outlook.com>
2022-05-01 18:04:07 +0300

    add redux

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ import Exhibitions from './components/exhibitions/Exhibitions';
 import Artists from './components/artists/Artists';
 import Artworks from './components/artworks/Artworks';
 import Footer from './components/footer/Footer';
-import ApiCall from './api/apiCall';
 import axios from 'axios';
 const baseUrl = 'https://api.artic.edu/api/v1/';
 
@@ -16,13 +15,27 @@ function App() {
   const [artists, setArtists] = useState([]);
   const [artistsImages, setArtistsImages] = useState([]);
 
+  // useEffect(() => {
+  //   let images = [];
+  //   artists
+  //     .filter((a) => a.artwork_ids !== null && a.death_date !== null)
+  //     .splice(0, 3)
+  //     .map((a) => {
+  //       const re = axios.get(
+  //         `https://api.artic.edu/api/v1/artworks/${a.artwork_ids[0]}`,
+  //       );
+  //       re.then((response) => {
+  //         images.push(response.data.data);
+  //       });
+  //       setArtistsImages(images);
+  //     });
+  // }, [artists]);
   useEffect(() => {
     const dataResponse = axios.get(baseUrl + 'exhibitions?limit=20');
     const artworksResponse = axios.get(baseUrl + 'artworks');
     const artistsResponse = axios.get(
       'https://api.artic.edu/api/v1/artists?limit=100/',
     );
-    let images = [];
     axios.all([dataResponse, artworksResponse, artistsResponse]).then(
       axios.spread((...responses) => {
         const responseOne = responses[0];
@@ -39,18 +52,18 @@ function App() {
             .filter((a) => a.artwork_ids !== null && a.death_date !== null)
             .splice(0, 3),
         );
-        responseThree.data.data
-          .filter((a) => a.artwork_ids !== null && a.death_date !== null)
-          .splice(0, 3)
-          .map((a) => {
-            const re = axios.get(
-              `https://api.artic.edu/api/v1/artworks/${a.artwork_ids[0]}`,
-            );
-            re.then((response) => {
-              images.push(response.data.data);
-            });
-            setArtistsImages(images);
-          });
+        // responseThree.data.data
+        //   .filter((a) => a.artwork_ids !== null && a.death_date !== null)
+        //   .splice(0, 3)
+        //   .map((a) => {
+        //     const re = axios.get(
+        //       `https://api.artic.edu/api/v1/artworks/${a.artwork_ids[0]}`,
+        //     );
+        //     re.then((response) => {
+        //       images.push(response.data.data);
+        //     });
+        //     setArtistsImages(images);
+        //   });
       }),
     );
   }, []);
--- src/components/artists/Artists.jsx
+++ src/components/artists/Artists.jsx
@@ -1,112 +1,69 @@
 import React, { useState, useEffect } from 'react';
 import Image from '../general/Image';
+import _ from 'lodash';
 import axios from 'axios';
 
-class Artists extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { images: props.images, artists: props.artists };
-  }
-  componentDidMount() {
-    this.setState(this.images);
-  }
-  render() {
-    return (
-      <section className="card-section flex flex--column" id="exhibitions">
-        <div className="section-header flex">
-          <div className="header-title">
-            <h2>Artists</h2>
-          </div>
-          <div className="header-link flex">
-            <a href="#" className="section-link">
-              All Artists
-            </a>
-            <i className="ri-arrow-right-line" />
-          </div>
+const Artists = (props) => {
+  const [artworks, setArtworks] = useState([]);
+  useEffect(() => {
+    let images = [];
+    if (props.artists) {
+      props.artists.map((art) => {
+        axios
+          .get(`https://api.artic.edu/api/v1/artworks/${art.artwork_ids[0]}`)
+          .then((response) => {
+            return images.push(response.data.data);
+          });
+        setArtworks(images);
+      });
+    }
+  }, [props.artists]);
+  return (
+    <section className="card-section flex flex--column" id="exhibitions">
+      <div className="section-header flex">
+        <div className="header-title">
+          <h2>Artists</h2>
         </div>
-        <div className="card-list grid">
-          {this.state.artists.map((el, index) => (
-            <div key={el.id} className="card flex flex--column">
-              <div className="card-header flex flex--column">
-                {this.state.images ? (
-                  <Image
-                    key={this.images[index].id}
-                    id={this.images[index].image_id}
-                    alt={this.images[index].title}
-                  />
-                ) : null}
-              </div>
-              <div className="card-body flex flex--column">
-                <h3>{el.title}</h3>
-                <p>
-                  <span>Birth Date:</span> {el.birth_date}
-                </p>
-                <p>
-                  <span>Death Date:</span> {el.death_date}
-                </p>
-              </div>
-            </div>
-          ))}
-        </div>
-
-        <div className="header-link hidden flex">
+        <div className="header-link flex">
           <a href="#" className="section-link">
             All Artists
           </a>
           <i className="ri-arrow-right-line" />
         </div>
-      </section>
-    );
-  }
-}
+      </div>
+      <div className="card-list grid">
+        {props.artists.map((el, index) => (
+          <div key={el.id} className="card flex flex--column">
+            <div className="card-header flex flex--column">
+              {!_.isEmpty(artworks.artworks) ? (
+                <Image
+                  key={artworks[index].id}
+                  id={artworks[index].image_id}
+                  alt={artworks[index].title}
+                />
+              ) : null}
+            </div>
+            <div className="card-body flex flex--column">
+              <h3>{el.title}</h3>
+              <p>
+                <span>Birth Date:</span> {el.birth_date}
+              </p>
+              <p>
+                <span>Death Date:</span> {el.death_date}
+              </p>
+            </div>
+          </div>
+        ))}
+      </div>
 
-// const Artists = (props) => {
-//   return (
-//     <section className="card-section flex flex--column" id="exhibitions">
-//       <div className="section-header flex">
-//         <div className="header-title">
-//           <h2>Artists</h2>
-//         </div>
-//         <div className="header-link flex">
-//           <a href="#" className="section-link">
-//             All Artists
-//           </a>
-//           <i className="ri-arrow-right-line" />
-//         </div>
-//       </div>
-//       <div className="card-list grid">
-//         {props.artists.map((el, index) => (
-//           <div key={el.id} className="card flex flex--column">
-//             <div className="card-header flex flex--column">
-//               {props.images.length > 0 ? (
-//                 <Image
-//                   key={props.images.images[index].id}
-//                   id={props.images.images[index].image_id}
-//                   alt={props.images.images[index].title}
-//                 />
-//               ) : null}
-//             </div>
-//             <div className="card-body flex flex--column">
-//               <h3>{el.title}</h3>
-//               <p>
-//                 <span>Birth Date:</span> {el.birth_date}
-//               </p>
-//               <p>
-//                 <span>Death Date:</span> {el.death_date}
-//               </p>
-//             </div>
-//           </div>
-//         ))}
-//       </div>
-
-//       <div className="header-link hidden flex">
-//         <a href="#" className="section-link">
-//           All Artists
-//         </a>
-//         <i className="ri-arrow-right-line" />
-//       </div>
-//     </section>
-//   );
-// };
+      <div className="header-link hidden flex">
+        <a href="#" className="section-link">
+          All Artists
+        </a>
+        <i className="ri-arrow-right-line" />
+      </div>
+    </section>
+  );
+};
 
 export default Artists;
